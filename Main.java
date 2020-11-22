c2ecf41452bfb574d234c44e8358902024fb3dbd
Kyle McDowell <kymcd21@icstudents.org>
2020-11-22 05:49:45 +0000

    why is it not stopping

--- Main.java
+++ Main.java
@@ -2,17 +2,19 @@ class Main {
   public static void main(String[] args) {
     for (int x = 2; x <= 20; x = x + 2)
       System.out.print(x + ",");
-
+    int y = 2;
+    int z = 2;
+    System.out.println();
     do {
-      int y = y + 2;
-      System.out.println(y + ",");
+      System.out.print(y + ",");
+      y = y + 2;
     } while (y <= 20);
-
-    while (z <= 20)
-      ;
+System.out.println();
+    while (z <= 20);
     {
-      int z = z + 2;
-      System.out.println(z + ",");
+      System.out.print(z + ",");
+      z = z + 2;
     }
+    System.out.println();
   }
 }
