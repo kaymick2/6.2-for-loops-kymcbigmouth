a06a696080e26e68c1226736e5001cc50b9181a2
Kyle McDowell <kymcd21@icstudents.org>
2020-11-22 05:43:05 +0000

    added other loops

--- Main.java
+++ Main.java
@@ -1,8 +1,18 @@
 class Main {
   public static void main(String[] args) {
-    for(int x = 0; x<=20;x=x+2)
-    System.out.print(x+",");
+    for (int x = 2; x <= 20; x = x + 2)
+      System.out.print(x + ",");
 
-    
+    do {
+      int y = y + 2;
+      System.out.println(y + ",");
+    } while (y <= 20);
+
+    while (z <= 20)
+      ;
+    {
+      int z = z + 2;
+      System.out.println(z + ",");
+    }
   }
 }
